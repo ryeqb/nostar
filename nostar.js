9f216fb5b613981d7a2ee2403efb00837c36b804
ryeqb <me@ryeqb.xyz>
2020-06-10 02:41:22 +0200

    Implement activity feed cleaning

--- nostar.js
+++ nostar.js
@@ -5,7 +5,8 @@
 // @description  Stop starring your own repos kekw
 // @author       Rye#0001
 // @match        https://github.com/Washi1337*
-// @grant        none
+// @match        https://github.com/
+// @grant        unsafeWindow
 // @runat        document-end
 // ==/UserScript==
 
@@ -14,14 +15,15 @@
 
     function noStar() {
     	try {
+            //Remove star from the main repo page
     		let pageStar = document.getElementsByClassName("js-toggler-container js-social-container starring-container ")[0];
 
     		if(pageStar)
     			pageStar.remove();
-
-    	} catch(err) { console.log(err); }
+    	} catch(err) {  }
 
     	try {
+            //Remove stars from the repository tab
     		let repositories = document.getElementById("user-repositories-list").getElementsByTagName("li");
 
     		if(repositories) {
@@ -30,10 +32,27 @@
     				tabStar.remove();
     			}
     		}
+    	} catch (err) {  }
 
-    	} catch (err) { console.log(err); }
+        try {
+            // Remove stars from the activity feed
+            let news = document.getElementsByClassName("news")[0];
+            let starNotice = news.getElementsByClassName("watch_started");
 
-    	console.log("removed!");
+            for(let i = 0; i < starNotice.length; i++) {
+                let username = "Washi1337";
+                let starElement = starNotice[i].getElementsByClassName("btn btn-sm ml-2 mb-2 js-toggler-target")[1];
+                let name = starNotice[i].getElementsByClassName("f4 lh-condensed text-bold text-gray-dark")[0].outerText.substring(0, username.length);
+
+                if(starElement && (name == username)) {
+                    starElement.remove();
+                    console.log(`Removed star index ${i} for ${username}`);
+                }
+            }
+
+        } catch(err) {  }
+
+    	console.log("Removed!");
     	return;
     }
 
